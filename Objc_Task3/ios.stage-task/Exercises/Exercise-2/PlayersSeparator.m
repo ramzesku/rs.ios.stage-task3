2c5d99ad1bc92a6729811e7db20488238cde6df5
Siarhei Lysiuk <siarhei.lysiuk@p97.com>
2021-05-24 20:58:10 +0300

    feat: complete exercise-2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,21 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int count = 0;
+    long countArray = (long) ratingArray.count;
+    for (int i = 0; i < countArray - 2; i++) {
+        for (int j = i + 1; j < countArray - 1; j++) {
+            for (int k = j + 1; k < countArray; k++) {
+                NSComparisonResult com1 = [ratingArray[i] compare:ratingArray[j]];
+                NSComparisonResult com2 = [ratingArray[j] compare:ratingArray[k]];
+                if (com1 == com2 && com1 != NSOrderedSame) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
 
 @end
